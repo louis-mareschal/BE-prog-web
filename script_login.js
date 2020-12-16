ac86eefc518687c147f5a4791dabf61134d6dcf4
Noecv <76057889+Noecv@users.noreply.github.com>
2020-12-16 11:32:57 +0100

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -157,7 +157,7 @@ function connecter(personne){
     $(register).css('display', 'block');
     $(login).css('display', 'none');
     localStorage.setItem("mode",personne.pseudo);
-    window.location='/';
+    window.location='index.html';
 }
 
 // Permet de générer la table
