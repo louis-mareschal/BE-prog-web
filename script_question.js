63c4064daf2b8504d9d10db6364f43fc067aeee3
louis-mareschal <76059946+louis-mareschal@users.noreply.github.com>
2020-12-16 11:42:40 +0100

    Update script_question.js

--- script_question.js
+++ script_question.js
@@ -14,25 +14,21 @@ Storage.prototype.keyindex = function(index) {
 }
 
 const liste_question = [
-    [new Question("Quel est le sportif le mieux payé ?","Cristiano Ronaldo","Floyd Mayweather","Lebron James","Roger Federer",4),
-        new Question("Quel est le sportif le plus médaillé des J.O. ?","Michael Phelps","Usain Bolt","Rafael Nadal","Martin Fourcade",1),
-        new Question("Quel est le sportif le plus riche ?","Tiger Woods","Lionel Messi","Roger Federer","Michael Jordan",4),
-        new Question("Basket : à quelle hauteur se situe le panier ?","2m95","3m05","3m15","3m25",2),
-        new Question("Dans quel sport un club français a-t-il remporté une Coupe d’Europe pour la première fois ?","Basketball","Football","Handball","Volleyball",4),
-        new Question("Judo : quel est le grade le plus élevé parmi ces ceintures ?","Bleue","Verte","Orange","Jaune",1),
-        new Question("Basketball : quelle taille mesurait Muggsy Bogues, le plus petit joueur de l’histoire de la NBA ?","1m59","1m62","1m64","1m67",1),
-        new Question("Où auront lieu les Jeux Olympiques 2022 ?","Almaty","Oslo","Pékin","Stockholm",3),
-        new Question("Quelle discipline fera son apparition à Paris 2024 ?","Breakdance","Escalade","Skateboard","Surf",1),
-        new Question("Le 15 avril 1993, ce club français fut le premier à gagner la plus prestigieuse coupe d'Europe, tous sports confondus ?","Olympique de Marseille","Chambéry","CSP Limoges","Paris SG-Asnières",3),
-        new Question("Durant la décennie 90, Michael Jordan et les Chicago Bulls ont remportés","6 titres NBA","7 titres NBA","8 titres NBA","5 titres NBA",1),
-        new Question("Michael Schumacher détient le record de titres de champion du monde de Formule 1 avec","5 titres","7 titres","6 titres","8 titres",2),
-        new Question("L'alsacien Sébastien Loeb détient le record du nombre de titres de champion du monde de rallye avec","7 titres","9 titres","6 titres","8 titres",2),
-        new Question("Quelle est la vitesse de pointe de Usain Bolt ?","37,58 km/h","44,72 km/h","46,24 km/h","39,56km/h",2),],
-    [new Question("Question 1","a","b","c","d",1),
-        new Question("Question 2","a","b","c","d",2),
-        new Question("Question 3","a","b","c","d",3),
-        new Question("Question 4","a","b","c","d",4)]
-] ;
+[new Question("Quel est le sportif le mieux payé ?","Cristiano Ronaldo","Floyd Mayweather","Lebron James","Roger Federer",4),
+new Question("Quel est le sportif le plus médaillé des J.O. ?","Michael Phelps","Usain Bolt","Rafael Nadal","Martin Fourcade",1),
+new Question("Quel est le sportif le plus riche ?","Tiger Woods","Lionel Messi","Roger Federer","Michael Jordan",4),
+new Question("Basket : à quelle hauteur se situe le panier ?","2m95","3m05","3m15","3m25",2),
+new Question("Judo : quel est le grade le plus élevé parmi ces ceintures ?","Bleue","Verte","Orange","Jaune",1),
+new Question("Où auront lieu les Jeux Olympiques 2022 ?","Almaty","Oslo","Pékin","Stockholm",3),
+new Question("Quelle est la vitesse de pointe de Usain Bolt ?","37,58 km/h","44,72 km/h","46,24 km/h","39,56km/h",2)],
+[new Question("Dans quel sport un club français a-t-il remporté une Coupe d’Europe pour la première fois ?","Basketball","Football","Handball","Volleyball",4),
+new Question("Le 15 avril 1993, ce club français fut le premier à gagner la plus prestigieuse coupe d'Europe, tous sports confondus ?","Olympique de Marseille","Chambéry","CSP Limoges","Paris SG-Asnières",3),
+new Question("Durant la décennie 90, Michael Jordan et les Chicago Bulls ont remportés","6 titres NBA","7 titres NBA","8 titres NBA","5 titres NBA",1),
+new Question("Michael Schumacher détient le record de titres de champion du monde de Formule 1 avec","5 titres","7 titres","6 titres","8 titres",2),
+new Question("L'alsacien Sébastien Loeb détient le record du nombre de titres de champion du monde de rallye avec","7 titres","9 titres","6 titres","8 titres",2),
+new Question("Basketball : quelle taille mesurait Muggsy Bogues, le plus petit joueur de l’histoire de la NBA ?","1m52","1m59","1m64","1m67",2),
+new Question("Quelle discipline fera son apparition à Paris 2024 ?","Breakdance","Escalade","Skateboard","Surf",1)]
+];
 numero_question = -1;
 numero_quiz = localStorage.getItem("numero_quiz");
 console.log(numero_quiz);
